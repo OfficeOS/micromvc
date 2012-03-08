0b31324b422e02d8c50be3fe5644828066353b1d
Corrie Zhao <hfcorriez@gmail.com>
2012-03-09 01:22:27 +0800

    Improve file logger

--- class/core/logger.php
+++ class/core/logger.php
@@ -1,38 +1,41 @@
 <?php
 /**
- * 日志
- * @author corriezhao
- *
+ * Logger
+ * @author corriezhao <hfcorriez@gmail.com>
  */
 
 namespace Core;
 
-abstract class Logger {
-    protected $messages = array();
-    protected $level = 0;
-    
+abstract class Logger
+{
+    protected $_messages = array();
+    protected $_level = 0;
+    protected $_sync = false;
+
     const LEVEL_DEBUG = 0;
     const LEVEL_NOTICE = 1;
     const LEVEL_WARN = 2;
     const LEVEL_ERROR = 3;
     const LEVEL_CRITICAL = 4;
-    
+
     private static $levels = array(
-        self::LEVEL_DEBUG => 'debug',
-    	self::LEVEL_WARN => 'warn',
-    	self::LEVEL_NOTICE =>'notice',
-    	self::LEVEL_ERROR => 'error',
-    	self::LEVEL_CRITICAL => 'critical',
+        self::LEVEL_DEBUG    => 'debug',
+        self::LEVEL_WARN     => 'warn',
+        self::LEVEL_NOTICE   => 'notice',
+        self::LEVEL_ERROR    => 'error',
+        self::LEVEL_CRITICAL => 'critical',
     );
-    
+
     /**
      * factroy a logger
+     *
      * @param string $name
+     *
      * @return Logger
      */
     public static function factory($name = 'default')
     {
-        if(!$config = Config::get('logger.' . $name))
+        if (!$config = Config::get('logger.' . $name))
         {
             throw new Exception('Config not exists for logger->' . $name);
         }
@@ -42,58 +45,80 @@ abstract class Logger {
 
     /**
      * instance a logger
+     *
      * @param string $name
+     *
      * @return Logger
      */
     public static function instance($name = 'default')
     {
         static $instance = array();
-        if(!isset($instance[$name]))
+        if (!isset($instance[$name]))
         {
             $instance[$name] = self::factory($name);
         }
         return $instance[$name];
     }
 
-    public function log($msg, $level = self::LEVEL_NOTICE) 
+    public function debug($msg)
     {
-        if($level < $this->level)
+        $this->log($msg, self::LEVEL_DEBUG);
+    }
+
+    public function warn($msg)
+    {
+        $this->log($msg, self::LEVEL_WARN);
+    }
+
+    public function notice($msg)
+    {
+        $this->log($msg, self::LEVEL_NOTICE);
+    }
+
+    public function error($msg)
+    {
+        $this->log($msg, self::LEVEL_ERROR);
+    }
+
+    public function critical($msg)
+    {
+        $this->log($msg, self::LEVEL_CRITICAL);
+    }
+
+    public function log($msg, $level = self::LEVEL_NOTICE)
+    {
+        if ($level < $this->_level)
         {
             return;
         }
+        $microtime = microtime(true);
         $message = array(
-            'id' => isset($_SERVER['HTTP_REQUEST_ID']) ? $_SERVER['HTTP_REQUEST_ID'] : '',
-            'type' => 'message',
-            'time' => microtime(true),
-            'text' => $msg,
-            'level' => self::$levels[$level],
-            'ip' => $_SERVER['REMOTE_ADDR'],
-            'memory' => memory_get_usage(),
+            'id'       => isset($_SERVER['HTTP_REQUEST_ID']) ? $_SERVER['HTTP_REQUEST_ID'] : '',
+            'time'     => $microtime,
+            'text'     => $msg,
+            'level'    => self::$levels[$level],
+            'ip'       => $_SERVER['REMOTE_ADDR'],
+            'memory'   => memory_get_usage(),
+            'datetime' => date('Y-m-d H:i:s', round($microtime)) . substr($microtime - floor($microtime), 1, 4),
+            'date'     => date('Y-m-d', round($microtime)),
         );
-        $this->messages[] = $message;
-    }
-    
-    public function getMessages(){
-        return $this->messages;
-    }
-    
-    public function __call($method, $params)
-    {
-        if(($level = array_search($method, self::$levels)) !== false)
+        $this->_messages[] = $message;
+        if ($this->_sync)
         {
-            $this->log($params[0], $level);
+            $this->write();
+            unset($this->_messages);
         }
-        else
-       {
-            throw new \BadMethodCallException('Bad Mehtod Call on Logger->' . $method);    
-        }
-        
+    }
+
+    public function getMessages()
+    {
+        return $this->_messages;
     }
 
     abstract protected function write();
 
-    function __destruct() {
-        //析构时写入
+    function __destruct()
+    {
         $this->write();
     }
 }
--- class/core/logger/file.php
+++ class/core/logger/file.php
@@ -1,46 +1,51 @@
 <?php
 /**
  * File Logger
- * @author corriezhao
+ *
+ * @author corriezhao <hfcorriez@gmail.com>
  *
  */
 
 namespace Core\Logger;
 
-class File extends \Core\Logger {
+class File extends \Core\Logger
+{
     private $_dir = '';
-    
-    public function __construct($config) {
+    private $_prefix = '';
+    private $_filename = ':date/:level.log';
+    private $_message = '[:datetime] :text #:id';
+
+    public function __construct($config)
+    {
         $this->_dir = $config['dir'];
-        $this->level = $config['level'];
+        $this->_prefix = $config['prefix'];
+        if (!empty($config['filename'])) $this->_filename = $config['filename'];
+        if (!empty($config['message'])) $this->_message = $config['message'];
+        $this->_level = $config['level'];
+        if (isset($config['sync'])) $this->_sync = $config['sync'];
     }
 
-    protected function write(){
-        if(!empty($this->messages))
+    protected function write()
+    {
+        if (empty($this->_messages)) return;
+
+        $dirname_exists = array();
+
+        foreach ($this->_messages as $message)
         {
-            $dir = $this->_dir;
-            if (!is_dir($dir)) 
+            $replace = array();
+            foreach ($message as $k=> $v) $replace[':' . $k] = $v;
+
+            $message_text = strtr($this->_message, $replace);
+            $filename = rtrim($this->_dir, '/') . '/' . strtr($this->_filename, $replace);
+            $dirname = dirname($filename);
+            if (!in_array($dirname, $dirname_exists) && !is_dir($dirname))
             {
-                mkdir($dir, 0777);
-                chmod($dir, 0777);
+                mkdir($dirname, 0777, true);
+                $dirname_exists[] = $dirname;
             }
-            $dir = $this->_dir . '/' . date('Ymd');
-            if (!is_dir($dir)) 
-            {
-                mkdir($dir, 0777);
-                chmod($dir, 0777);
-            }
-            $messages_wraper = array();
-            foreach($this->messages as $message)
-            {
-                $messages_wraper[$message['level']][] = '[' . date('Y-m-d H:i:s', $message['time']) . '] ' . ($message['id'] ? "#{$message['id']} " : '') . $message['text'];
-            }
-            
-            foreach ($messages_wraper as $level => $wraper)
-            {
-                $file = $this->_dir . '/' . date('Ymd') . '/' . $level . '.log';
-                file_put_contents($file, join(PHP_EOL, $wraper) . PHP_EOL, FILE_APPEND);
-            }
+
+            file_put_contents($filename, $message_text . PHP_EOL, FILE_APPEND);
         }
     }
 
